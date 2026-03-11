e29ed0ab04eeca2e5d982e71df59e16ed1e00f33
Suman Yongya <sing85483@gmail.com>
2026-03-11 22:09:10 +0545

    Create First code.c
    U can start your coding journey from this helloworld code

---
+++ First code.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>//header file inclusion or Pre-processor Directive
+
+ int main() { //main function or thinks has front door
+     printf("Helloworld!"); function call for output
+     return 0; //return statement
+ }
+     
+
+
+
+
+     
